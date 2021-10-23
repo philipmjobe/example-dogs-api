7812929ebb19636afaf27a4294d0fd2906b481d0
Philip <philipmjobe@gmail.com>
2021-10-23 14:36:37 -0400

    added create method in dog_controller.rb

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,19 @@
 class DogsController < ApplicationController
+
+  def create
+    if current_user
+      dog = Dog.new(
+        name: params["name"]
+        age: params["age"]
+        breed: params["breed"]
+      )
+      if dog.save
+        render json: dog.as_json
+      else  
+        render json: {error: dog.errors.full_messages}
+      end 
+    else  
+      render json: {error: "Please log in"}
+    end 
+  end 
 end
