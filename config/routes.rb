cbed7d7a6b5c4fe8cf97a1c143fdd9c632e835c5
Philip <philipmjobe@gmail.com>
2021-10-23 14:30:37 -0400

    added jwt gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "/users" => "users#create"
 
-  
-  
-  
   post "/dogs" => "dogs#create"
+
+  post "/sessions" => "sessions#create"
+  
+  
+  
 end
